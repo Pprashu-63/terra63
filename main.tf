52b42e138f620e2fee7824de448af1b83f3f49ac
prashanth <prasanth.kagupati@gmail.com>
2023-07-27 16:33:45 +0530

    latest

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my_instance" {
   instance_type = "t3.medium"
   count=5 
   key_name      = "new-kp"
-  vpc_security_group_ids = ["sg-023aab4c9cbab376a"]
+  vpc_security_group_ids = ["sg-00b034fdef97af84d"]
   subnet_id     = "subnet-0c0ab16ad88c499ba"
   tags= {
   Name: "Terra-jen"
