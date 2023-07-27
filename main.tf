578d6b6a08cccce94d4f0445be7025d9679cc2ad
Kagupati Prashanth <86354891+Pprashu-63@users.noreply.github.com>
2023-07-27 18:06:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyVPC63"
+  }
 }
 
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "sub63"
+  }
 }
 
 
