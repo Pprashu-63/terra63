7f9c3ca5d15fd59260e09b8342f657fb402bf97d
prashanth <prasanth.kagupati@gmail.com>
2023-07-28 12:16:48 +0530

    dynamodb

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "my_instance" {
+  ami           = "ami-05548f9cecf47b442"
+  instance_type = "t3.medium"
+  key_name      = "new-kp"
+  vpc_security_group_ids = ["sg-00b034fdef97af84d"]
+  subnet_id     = "subnet-0c0ab16ad88c499ba"
+  tags= {
+  Name: "Terra63s"
+}
+}
